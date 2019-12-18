59ff4e3d58690ec4565c23c1b28d44d3c5828246
BJW2K\athanislaus <athanislaus@bjs.com>
2019-12-18 17:17:34 -0500

    Adding JavaDoc#

--- src/main/java/com/movie/service/MovieServiceCmdImpl.java
+++ src/main/java/com/movie/service/MovieServiceCmdImpl.java
@@ -14,6 +14,10 @@ public class MovieServiceCmdImpl implements MovieServiceCmd {
     @Autowired
     private MovieRepository movieRepo;
 
+    /**
+     * Method will add movie  details to H2 DataBase
+     * @param movie
+     */
     @Override
     public void addMovies(Movie movie) {
         // add movies
@@ -25,12 +29,20 @@ public class MovieServiceCmdImpl implements MovieServiceCmd {
         movieRepo.save(inpData);
     }
 
+    /**
+     * Mehtod to retrieve all movie details
+     * @return
+     */
     @Override
     public List<MovieTable> getMovies() {
         List<MovieTable> results = movieRepo.findAll();
         return results;
     }
 
+    /**
+     * Delete movie details by movie#
+     * @param id
+     */
     @Override
     public void deleteMvoie(Integer id) {
         movieRepo.deleteById(id);
